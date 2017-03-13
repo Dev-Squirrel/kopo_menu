ac91e60b1ea17c63cea3940a749f7882530d45e2
Dev-Squirrel <youdienet@gmail.com>
2017-03-14 01:05:00 +0900

    Add exit code

--- kopo_menu/src/Main.java
+++ kopo_menu/src/Main.java
@@ -5,6 +5,14 @@ public class Main{
 		
 		// parsing 클래스에 parsing_call 메서드 불려오기
 		parsing call = new parsing();
-		call.parsing_call();	
+		call.parsing_call();
+		
+		// 콘솔 프로그램 엔터 치면 종료
+		System.out.println("엔터를 치면 프로그램이 종료됩니다.");
+			try {
+				System.in.read();
+			} catch (Exception ex) {
+				ex.printStackTrace();
+			}
 	}
 }
